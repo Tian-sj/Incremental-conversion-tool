11cf0bd0e0c85bbeec3bf849fcfd5e8dfd6726e2
Tian_sj <tsj3911@163.com>
2022-09-08 09:47:07 +0800

    美化

--- InConTool.pro
+++ InConTool.pro
@@ -6,17 +6,19 @@ win32-msvc*:QMAKE_CXXFLAGS += /utf-8
 # QMAKE_LFLAGS += "/MANIFESTUAC:\"level='requireAdministrator' uiAccess='false'\""
 
 SOURCES += \
-    $$files($$PWD/src/*.cpp)
+    src/main.cpp \
+    src/InConTool.cpp
 
 HEADERS += \
-    $$files($$PWD/src/*.h)
+    src/InConTool.h
 
-FORMS += $$PWD/src/InConTool.ui
+FORMS += \
+    src/InConTool.ui
 
 # Default rules for deployment.
-qnx: target.path = /tmp/$${TARGET}/bin
-else: unix:!android: target.path = /opt/$${TARGET}/bin
-!isEmpty(target.path): INSTALLS += target
+    qnx: target.path = /tmp/$${TARGET}/bin
+    else: unix:!android: target.path = /opt/$${TARGET}/bin
+    !isEmpty(target.path): INSTALLS += target
 
 RESOURCES += \
     resource.qrc
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "InConTool.h"
+#include <QTextCodec>
 
 #include <QApplication>
 #pragma comment(lib, "user32.lib")
@@ -6,6 +7,25 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //设置中文字体
+    a.setFont(QFont("Microsoft Yahei", 9));
+
+    //设置中文编码
+#if (QT_VERSION <= QT_VERSION_CHECK(5, 0, 0))
+#if _MSC_VER
+    QTextCodec *codec = QTextCodec::codecForName("GBK");
+#else
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+#endif
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
+#else
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
+#endif
+
     InConTool w;
     w.show();
     return a.exec();
